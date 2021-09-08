803af0ec94d6eb33399ba4c9283e766bc5c67600
Fajar-sp <89977907+Fajar-sp@users.noreply.github.com>
2021-09-08 20:59:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const menu = require("./lib/menu.js")
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
 const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
-const aktif = 'Kapan bot aktif';
+const  instagram  =  'Mhdfjrpratama_16 kaelMk_' ;
+const  aktif  =  'Kapan bot aktif' ;
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
+            + 'FN:FAJAR\n' // Nama kamu
+            + 'ORG:NOTBOT;\n' // Nama bot
             + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
